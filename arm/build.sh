491cb4661daddedfd50c463ed61fff80b459f430
Leo Lee <gb8l805@gmail.com>
2018-10-29 02:51:58 +0800

    Release 4.1.3

--- arm/build.sh
+++ arm/build.sh
@@ -57,7 +57,7 @@ do_build() {
 }
 
 do_backup(){
-	HISTORY_DIR="../../zhiopi_history_package/arm"
+	HISTORY_DIR="/root/arm/zhiopi_history_package/arm"
 	# backup latested package after pack
 	backup_version=`cat version | sed -n 1p`
 	backup_tar_md5=`cat version | sed -n 2p`
