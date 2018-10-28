c3f25221c62a3b12df6b1bdfd64dbe6adddb9e50
Leo Lee <gb8l805@gmail.com>
2018-10-29 01:57:59 +0800

    ver 4.1.3

--- arm/build.sh
+++ arm/build.sh
@@ -57,7 +57,7 @@ do_build() {
 }
 
 do_backup(){
-	HISTORY_DIR="../../fancyss_history_package/fancyss_arm"
+	HISTORY_DIR="../../zhiopi_history_package/arm"
 	# backup latested package after pack
 	backup_version=`cat version | sed -n 1p`
 	backup_tar_md5=`cat version | sed -n 2p`
