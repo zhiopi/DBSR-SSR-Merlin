9d07f35ae03881a22a31915e1baae570396b5882
admin <admin@DESKTOP-2KGQ0BP>
2018-12-12 04:02:57 +0800

    update

--- arm/config.json.js
+++ arm/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2018-09-28_15:39:23",
+"build_date":"2018-11-25_22:15:09",
 "description":"菠萝云",
 "home_url":"Main_Ss_Content.asp",
-"md5":"e9e0333d1f5d7a471f5737bc03156004",
+"md5":"75a1791f7182580b753d417a201da534",
 "name":"shadowsocks",
-        "tar_url": "https://raw.githubusercontent.com/zhiopi/DBSR-SSR-Merlin/master/arm/shadowsocks.tar.gz", 
+"tar_url": "https://raw.githubusercontent.com/zhiopi/DBSR-SSR-Merlin/master/arm/shadowsocks.tar.gz", 
 "title":"菠萝云",
-"version":"4.1.3"
+"version":"4.1.7"
 }
--- arm/history/README.md
+++ arm/history/README.md
@@ -1,2 +1,2 @@
-# arm - 菠萝云离线安装包
-离线包下载请前往：[https://github.com/zhiopi/DBSR-SSR-Merlin/releases](https://github.com/zhiopi/DBSR-SSR-Merlin/releases)
+# Merlin_arm - 菠萝云离线安装包
+离线包下载请前往：[https://www.dbsr.cf](https://www.dbsr.cf)
