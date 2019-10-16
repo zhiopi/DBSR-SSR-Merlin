5ae995bc4fa7aa735ba36debcb6683f6fa6c6cec
zhiopi <gb8l805@gmail.com>
2019-10-16 16:45:12 +0800

    UPDATE

--- arm/config.json.js
+++ arm/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2019-06-20_15:25:20",
+"build_date":"2019-10-16_07:57:19",
 "description":"菠萝云",
 "home_url":"Main_Ss_Content.asp",
-"md5":"8a7c0b68f54c35427a071c537de7ec3a",
+"md5":"462a54580c2da65414fbd4cec54ab702",
 "name":"shadowsocks",
-"tar_url": "https://raw.githubusercontent.com/zhiopi/DBSR-SSR-Merlin/master/arm/shadowsocks.tar.gz", 
+"tar_url": "https://raw.githubusercontent.com/zhiopi/DBSR-SSR-Merlin/master/arm//shadowsocks.tar.gz", 
 "title":"菠萝云",
 "version":"4.2.0"
 }
